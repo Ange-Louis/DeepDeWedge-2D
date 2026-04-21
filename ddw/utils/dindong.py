4a48bafc4057c04d1dfecadef432202c00f702d9
Ange-Louis <al.sammarcelli@outlook.com>
2026-04-21 18:03:19 +0200

    Refactor prepare_data2 for MRC files; not working with mask files

--- ddw/utils/dindong.py
+++ ddw/utils/dindong.py
@@ -6,68 +6,29 @@ import mrcfile
 import numpy as np
 from PIL import Image
 
-def collect_data(image_file, output_dir):
-    try:
-        with mrcfile.open(image_file, permissive=True) as mrc:
-            try:
-                data = torch.tensor(mrc.data)
-            except TypeError:
-                data = torch.tensor(mrc.data.astype(float))
-            print(data.shape)
+from ddw.utils.mrctools2 import load_data
+standardize_full_tomos = True
+tomo0_tensorfiles = Path(f"/home/nathan/Desktop/Ange-Louis/DDW2/testing/tomos/tomo0").glob("*.pt")
+tomo1_tensorfiles = Path(f"/home/nathan/Desktop/Ange-Louis/DDW2/testing/tomos/tomo1").glob("*.pt")
 
-            file_name = Path(image_file).stem
-            for y in range(data.shape[1]):
-                torch.save(data[:, y, :].clone(), f"{output_dir}/{file_name}_{y}.pt")
-                print(f"Sauvegarde du fichier '{file_name}_({y}).pt'. Taille : {os.path.getsize(f"{output_dir}/{file_name}_{y}.pt")}")
-
-    except: 
-        try:
-            # Essayer de charger avec PIL (pour les formats standards)
-            img = Image.open(image_file)
-            data = np.array(img)
-            data = torch.tensor(data)
-
-            file_name =  Path(image_file).stem
-            torch.save(data.clone(), f"{output_dir}/{file_name}.pt")
-            print(f"Sauvegarde du fichier '{file_name}.pt'. Taille : {os.path.getsize(file_name)}")
-            
-        except Exception as e:
-            raise ValueError(f"Error: {e}")
-        
-
-def setup_tomo_dir(project_dir, tomo_dir = None, overwrite = True, verbose =True):
-    """
-    Sets up and manages directories for storing tomogram and subtomogram data
-    """
-    if tomo_dir is None:
-        if project_dir is not None:
-            tomo_dir = f"{project_dir}/tomos"
-        else:
-            raise ValueError(
-                "tomo_dir must be provided if project_dir is not provided"
-            )
-    if verbose:
-        print(f"Saving all tomogram tensors to '{tomo_dir}'.")
-    if os.path.exists(tomo_dir):
-        if overwrite == True:
-            if verbose:
-                print(f"Removing existing tomogram directory '{tomo_dir}'.")
-            shutil.rmtree(tomo_dir)
-        else:
-            raise ValueError(
-                f"subtomo_dir '{tomo_dir}' already exists. Set 'overwrite' to 'True' to remove it."
-            )
-
-    os.makedirs(f"{tomo_dir}/tomo0/", exist_ok=False)
-    os.makedirs(f"{tomo_dir}/tomo1/", exist_ok=False)
-
-    return tomo_dir
-
-
-tomo0_files = ["/home/nathan/Desktop/Ange-Louis/Dataset/DDW_tutorial/tomo_all_frames.rec"]
-tomo_dir = setup_tomo_dir(project_dir= "testing")
-
-for tomo0_file in tomo0_files:
-	
-    collect_data(tomo0_file, output_dir= f"{tomo_dir}/tomo0")
+fitting_counter, val_counter = 0, 0
+# sans masque car je ne sais pas le faire
+for k, (tomo0_tensorfile, tomo1_tensorfile) in enumerate(zip(tomo0_tensorfiles, tomo1_tensorfiles)):
+	#print(k, tomo0_tensorfile, tomo1_tensorfile)
+    tomo0 = load_data(tomo0_tensorfile).float()
+    tomo1 = load_data(tomo1_tensorfile).float()
+    if standardize_full_tomos:
+        print(
+            f"Standardizing tomogram '{Path(tomo0_tensorfile).stem}' before extracting sub-tomograms."
+        )
+        tomo0 -= tomo0.mean(); tomo1 -= tomo1.mean()
+        tomo0 /= tomo0.std(); tomo1 /= tomo1.std()
+    else:
+        std = tomo0.std()
+        if std < 1e-3:
+            print(f"\
+                WARNING: Standard deviation of '{Path(tomo0_tensorfile).stem}' is low ({std}), which may lead to issues during model fitting!\
+                \nConsider setting 'standardize_full_tomos=True'.\
+                \nIf you do so, you must also set 'standardize_full_tomos=True' for 'ddw refine-tomogram'.\
+        ")
 
